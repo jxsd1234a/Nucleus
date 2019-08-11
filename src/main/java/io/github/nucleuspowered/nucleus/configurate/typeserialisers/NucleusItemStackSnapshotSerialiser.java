03166827da5f4e11a0662ef9b19429e2ff15635a
Daniel Naylor <git@drnaylor.co.uk>
2019-08-11 17:28:55 +0100

    Add some validation for kit saves/loads
    Part way to #1384

--- src/main/java/io/github/nucleuspowered/nucleus/configurate/typeserialisers/NucleusItemStackSnapshotSerialiser.java
+++ src/main/java/io/github/nucleuspowered/nucleus/configurate/typeserialisers/NucleusItemStackSnapshotSerialiser.java
@@ -6,18 +6,27 @@ package io.github.nucleuspowered.nucleus.configurate.typeserialisers;
 
 import com.google.common.collect.Lists;
 import com.google.common.reflect.TypeToken;
+import io.github.nucleuspowered.nucleus.Nucleus;
 import io.github.nucleuspowered.nucleus.configurate.wrappers.NucleusItemStackSnapshot;
+import io.github.nucleuspowered.nucleus.util.PrettyPrinter;
 import io.github.nucleuspowered.nucleus.util.TypeHelper;
 import ninja.leaping.configurate.ConfigurationNode;
+import ninja.leaping.configurate.objectmapping.ObjectMappingException;
 import ninja.leaping.configurate.objectmapping.serialize.TypeSerializer;
+import org.slf4j.event.Level;
 import org.spongepowered.api.data.DataContainer;
 import org.spongepowered.api.data.DataQuery;
+import org.spongepowered.api.data.DataView;
 import org.spongepowered.api.data.key.Keys;
+import org.spongepowered.api.data.persistence.DataFormats;
 import org.spongepowered.api.data.persistence.DataTranslators;
+import org.spongepowered.api.item.ItemType;
+import org.spongepowered.api.item.ItemTypes;
 import org.spongepowered.api.item.inventory.ItemStack;
 import org.spongepowered.api.item.inventory.ItemStackSnapshot;
 import org.spongepowered.api.util.Tuple;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -94,6 +103,28 @@ public class NucleusItemStackSnapshotSerialiser implements TypeSerializer<Nucleu
             return NucleusItemStackSnapshot.NONE;
         }
 
+        // Validate the item.
+        if (snapshot.isEmpty() || snapshot.getType() == ItemTypes.NONE) {
+            // don't bother
+            return NucleusItemStackSnapshot.NONE;
+        }
+
+        if (snapshot.getType() == null) {
+            // well, this isn't going to work, is it?
+            PrettyPrinter printer = new PrettyPrinter();
+            printer.add("Null item type recorded when trying to create an item stack").centre().hr();
+            printer.add("When trying to load item stacks for kits, a null item type was found. This can occur when changing packs and items no "
+                    + "longer exist. The item has been discarded.");
+            printer.add("Item Info:");
+            try {
+                printer.add(DataFormats.JSON.write(dataContainer));
+            } catch (IOException e) {
+                printer.add("Unable to write info");
+            }
+            printer.log(Nucleus.getNucleus().getLogger(), Level.WARN);
+            return NucleusItemStackSnapshot.NONE;
+        }
+
         if (emptyEnchant) {
             snapshot.offer(Keys.ITEM_ENCHANTMENTS, Lists.newArrayList());
             return new NucleusItemStackSnapshot(snapshot.createSnapshot());
@@ -109,8 +140,31 @@ public class NucleusItemStackSnapshotSerialiser implements TypeSerializer<Nucleu
     }
 
     @Override
-    public void serialize(TypeToken<?> type, NucleusItemStackSnapshot obj, ConfigurationNode value) {
-        DataContainer view = obj.getSnapshot().toContainer();
+    public void serialize(TypeToken<?> type, NucleusItemStackSnapshot obj, ConfigurationNode value) throws ObjectMappingException {
+        ItemStackSnapshot snapshot = obj.getSnapshot();
+        DataView view;
+        try {
+             view = snapshot.toContainer();
+        } catch (NullPointerException ex) {
+            PrettyPrinter printer = new PrettyPrinter();
+            // Sponge can't find an item type...
+            printer.add("NPE encountered when trying to save an item to a kit").centre().hr();
+            printer.add("When trying to save an item to the kit, Sponge could not turn the item into data Nucleus can save.");
+            printer.add("This can occur when changing packs and items no longer exist.");
+            printer.hr();
+            ItemType itemType = snapshot.getType();
+            if (itemType == null) {
+                printer.add("The ItemType was set to null, but is not class as an empty snapshot. It will not be saved.");
+            } else {
+                printer.add("The ItemType was set to {} (name: {}). It will not be saved.", itemType.getId(), itemType.getName());
+            }
+
+            printer.hr();
+            printer.add("Stack trace:");
+            printer.add(ex);
+            printer.log(Nucleus.getNucleus().getLogger(), Level.WARN);
+            return;
+        }
         Map<DataQuery, Object> dataQueryObjectMap = view.getValues(true);
         for (Map.Entry<DataQuery, Object> entry : dataQueryObjectMap.entrySet()) {
             if (entry.getValue().getClass().isArray()) {
