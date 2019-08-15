fe9178afef6e4162612ab02152cc050ed9b38fed
Daniel Naylor <git@drnaylor.co.uk>
2019-08-15 20:08:53 +0100

    Format for stylistic and performance changes

--- src/main/java/io/github/nucleuspowered/nucleus/modules/playerinfo/commands/NearCommand.java
+++ src/main/java/io/github/nucleuspowered/nucleus/modules/playerinfo/commands/NearCommand.java
@@ -4,19 +4,19 @@
  */
 package io.github.nucleuspowered.nucleus.modules.playerinfo.commands;
 
-import io.github.nucleuspowered.nucleus.Nucleus;
+import com.flowpowered.math.vector.Vector3d;
+import io.github.nucleuspowered.nucleus.Util;
 import io.github.nucleuspowered.nucleus.internal.annotations.command.Permissions;
 import io.github.nucleuspowered.nucleus.internal.annotations.command.RegisterCommand;
 import io.github.nucleuspowered.nucleus.internal.command.AbstractCommand;
-import io.github.nucleuspowered.nucleus.internal.command.NucleusParameters;
 import io.github.nucleuspowered.nucleus.internal.command.ReturnMessageException;
 import io.github.nucleuspowered.nucleus.internal.docgen.annotations.EssentialsEquivalent;
 import io.github.nucleuspowered.nucleus.internal.interfaces.Reloadable;
 import io.github.nucleuspowered.nucleus.internal.permissions.PermissionInformation;
 import io.github.nucleuspowered.nucleus.internal.permissions.SuggestedLevel;
 import io.github.nucleuspowered.nucleus.internal.services.PlayerOnlineService;
-import io.github.nucleuspowered.nucleus.modules.core.datamodules.CoreUserDataModule;
 import io.github.nucleuspowered.nucleus.modules.playerinfo.config.PlayerInfoConfigAdapter;
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
@@ -24,25 +24,33 @@ import org.spongepowered.api.command.args.CommandElement;
 import org.spongepowered.api.command.args.GenericArguments;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.entity.living.player.User;
-import org.spongepowered.api.event.cause.Cause;
-import org.spongepowered.api.service.pagination.PaginationList;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.action.TextActions;
+import org.spongepowered.api.util.Tuple;
 import org.spongepowered.api.util.annotation.NonnullByDefault;
 import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.World;
 
-import java.util.*;
+import java.text.NumberFormat;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Permissions(suggestedLevel = SuggestedLevel.USER, supportsOthers = true)
 @RegisterCommand("near")
 @EssentialsEquivalent("near")
 @NonnullByDefault
-public class NearCommand extends AbstractCommand<CommandSource> implements Reloadable {
+public class NearCommand extends AbstractCommand.SimpleTargetOtherUser implements Reloadable {
 
+    private static final NumberFormat formatter =  NumberFormat.getInstance();
     private final String radiusKey = "radius";
     private int maxRadius;
+
+    static {
+        formatter.setMaximumFractionDigits(2);
+    }
 
     @Override
     protected Map<String, PermissionInformation> permissionSuffixesToRegister() {
@@ -52,9 +60,8 @@ public class NearCommand extends AbstractCommand<CommandSource> implements Reloa
     }
 
     @Override
-    public CommandElement[] getArguments() {
+    protected CommandElement[] additionalArguments() {
         return new CommandElement[] {
-                GenericArguments.optionalWeak(requirePermissionArg(GenericArguments.user(Text.of(NucleusParameters.ONE_USER_PLAYER_KEY)), this.permissions.getOthers())),
                 GenericArguments.optionalWeak(GenericArguments.integer(Text.of(this.radiusKey)))
         };
     }
@@ -62,47 +69,61 @@ public class NearCommand extends AbstractCommand<CommandSource> implements Reloa
     //near radius
     //near player radius
     @Override
-    protected CommandResult executeCommand(CommandSource src, CommandContext args, Cause cause) throws Exception {
-        final User user = this.getUserFromArgs(User.class, src, NucleusParameters.Keys.PLAYER, args);
+    protected CommandResult executeWithPlayer(CommandSource src, User user, CommandContext args, boolean isSelf) throws Exception {
         final Location<World> location;
+        final Vector3d position;
         if (user.isOnline()) {
             location = user.getPlayer().get().getLocation();
+            position = location.getPosition();
         } else {
-            location = Nucleus.getNucleus().getUserDataManager().get(user).flatMap(x -> x.get(CoreUserDataModule.class).getLogoutLocation()).orElseThrow(() -> ReturnMessageException.fromKey("command.near.location.nolocation", user.getName()));
+            World world = user.getWorldUniqueId()
+                    .flatMap(x -> Sponge.getServer().getWorld(x))
+                    .orElseThrow((() -> ReturnMessageException.fromKey(src,"command.near.location.nolocation", user.getName())));
+            position = user.getPosition();
+            location = new Location<>(world, position);
         }
 
-        int radius = maxRadius;
-        final Optional<Integer> radiusOpt = args.getOne(radiusKey);
-        if(radiusOpt.isPresent()) {
+        int radius = this.maxRadius;
+        final Optional<Integer> radiusOpt = args.getOne(this.radiusKey);
+        if (radiusOpt.isPresent()) {
             int inputRadius = radiusOpt.get();
-            //Check if executer has max radius override permission
-            if (inputRadius > maxRadius && this.permissions.testSuffix(src, "maxexempt")) {
+            // Check if executor has max radius override permission
+            if (inputRadius > this.maxRadius && this.permissions.testSuffix(src, "maxexempt")) {
                 radius = inputRadius;
             } else {
-                src.sendMessage(Nucleus.getNucleus().getMessageProvider().getTextMessageWithFormat("command.near.radiustoobig", this.maxRadius));
+                sendMessageTo(src, "command.near.radiustoobig", this.maxRadius);
             }
         }
 
         final PlayerOnlineService playerOnlineService = getServiceManager().getServiceUnchecked(PlayerOnlineService.class);
-        final List<Player> nearbyPlayers = location.getExtent().getNearbyEntities(location.getPosition(), radius).stream()
-                .filter(Player.class::isInstance)
-                .map(Player.class::cast)
-                .filter(e -> e.getUniqueId() != user.getUniqueId() && playerOnlineService.isOnline(src, e))
-                .sorted((o1, o2) -> (int) o1.getLocation().getPosition().distanceSquared(o2.getPosition()))//Sort by distance
-                .collect(Collectors.toList());
+        final List<Text> messagesToSend =
+                location.getExtent()
+                        .getNearbyEntities(location.getPosition(), radius)
+                        .stream()
+                        .filter(Player.class::isInstance)
+                        .map(Player.class::cast)
+                        .filter(e -> e.getUniqueId() != user.getUniqueId() && playerOnlineService.isOnline(src, e))
+                        .map(x -> Tuple.of(x, location.getPosition().distance(x.getPosition())))
+                        .sorted(Comparator.comparingDouble(Tuple::getSecond))
+                        .map(tuple -> createPlayerLine(src, tuple))
+                        .collect(Collectors.toList());
 
-        final PaginationList.Builder listBuilder = PaginationList.builder().title(Nucleus.getNucleus().getMessageProvider().getTextMessageWithFormat("command.near.playersnear", user.getName()));
-        final List<Text> messages = new ArrayList<>(nearbyPlayers.size());
-        nearbyPlayers.forEach(nearbyPlayer -> {
-            Text.Builder line = Text.builder();
-            line.append(Nucleus.getNucleus().getMessageProvider().getTextMessageWithFormat("command.near.playerdistancefrom", nearbyPlayer.getName(), (int)Math.abs(nearbyPlayer.getLocation().getPosition().distance(location.getPosition()))))
-            .onClick(TextActions.runCommand("/tp "+nearbyPlayer.getName()))
-            .onHover(TextActions.showText(Nucleus.getNucleus().getMessageProvider().getTextMessageWithFormat("command.near.tpto", nearbyPlayer.getName())));
-            messages.add(line.build());
-        });
-        listBuilder.contents(messages).sendTo(src);
+        Util.getPaginationBuilder(src)
+                        .title(getMessageFor(src, "command.near.playersnear", user.getName()))
+                        .contents(messagesToSend)
+                        .sendTo(src);
 
         return CommandResult.success();
+    }
+
+    private Text createPlayerLine(CommandSource src, Tuple<Player, Double> player) {
+        Text.Builder line = Text.builder();
+        getMessageFor(src, "command.near.playerdistancefrom", player.getFirst().getName());
+        line.append(getMessageFor(src, "command.near.playerdistancefrom", player.getFirst().getName(),
+                formatter.format(Math.abs(player.getSecond()))))
+                .onClick(TextActions.runCommand("/tp " + player.getFirst().getName()))
+                .onHover(TextActions.showText(getMessageFor(src,"command.near.tpto", player.getFirst().getName())));
+        return line.build();
     }
 
     @Override
